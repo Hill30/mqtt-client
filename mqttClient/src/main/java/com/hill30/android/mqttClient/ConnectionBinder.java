f14efbefef31d4d08fe54be86ca35d50a75d91bf
Michael Feingold <mfeingold@hill30.com>
2014-02-06 17:15:49 -0600

    a little refactoring

--- mqttClient/src/main/java/com/hill30/android/mqttClient/ConnectionBinder.java
+++ mqttClient/src/main/java/com/hill30/android/mqttClient/ConnectionBinder.java
@@ -12,28 +12,20 @@ public class ConnectionBinder extends Binder {
     private final Connection connection;
     private final String inboundTopic;
     private final String outboundTopic;
-    private String topic;
     private ServiceConnection.MessageListener<String> messageListener;
 
     public ConnectionBinder(Connection connection, Intent intent) {
         this.connection = connection;
-        topic = intent.getStringExtra(Service.TOPIC_NAME);
+        String topic = intent.getStringExtra(Service.TOPIC_NAME);
         inboundTopic = topic + "/Inbound/user";
         outboundTopic = topic + "/Outbound";
     }
 
-//    @Override
-    public String getTopic() {
-        return topic;
-    }
-
-//    @Override
     public void connect() throws MqttException {
         connection.registerSubscriber(inboundTopic, this);
         connection.connect(inboundTopic);
     }
 
-//    @Override
     public void onMessageReceived(String message) {
         if (messageListener != null)
             messageListener.onMessageArrived(message);
