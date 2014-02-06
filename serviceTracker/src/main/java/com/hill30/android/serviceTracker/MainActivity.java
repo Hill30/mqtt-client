4c38ed1d32e3deddb698b743219cd9f6a2af03fe
Michael Feingold <mfeingold@hill30.com>
2014-02-06 11:20:39 -0600

    project cleanup

--- serviceTracker/src/main/java/com/hill30/android/serviceTracker/MainActivity.java
+++ serviceTracker/src/main/java/com/hill30/android/serviceTracker/MainActivity.java
@@ -2,8 +2,6 @@ package com.hill30.android.serviceTracker;
 
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
